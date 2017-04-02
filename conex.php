3f0d5aee0e51275bdd36d45ba1777929dae46543
Paola Blandino <blandinopao12@gmail.com>
2017-04-01 23:47:22 -0600

    Last from today

---
+++ conex.php
@@ -0,0 +1,7 @@
+<?php
+$hostname_localhost = "sql9.freemysqlhosting.net";
+$database_localhost = "sql9166993";
+$username_localhost = "sql9166993";
+$password_localhost = "iVXAe2Rd5t";
+$localhost = mysql_pconnect($hostname_localhost, $username_localhost, $password_localhost) or trigger_error(mysql_error(),E_USER_ERROR); 
+?>
